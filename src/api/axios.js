7afdd66f4a50a39579d6c8e148b55df862037c55
silverbladeidn <aribiya@gmail.com>
2026-07-12 11:25:08 +0700

    login problem : authentication 2

--- src/api/axios.js
+++ src/api/axios.js
@@ -25,7 +25,7 @@ api.interceptors.response.use(
     (error) => {
         if (error.response?.status === 401) {
             localStorage.removeItem('token');
-            window.location.href = '/login';
+            window.location.href = '/';
         }
         return Promise.reject(error);
     }
